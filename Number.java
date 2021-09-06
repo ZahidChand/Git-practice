b70721e69898f72779f790d9f234a04957e8197d
Zahid <chandzahid333@gmail.com>
2021-09-06 10:37:26 +0530

    initial commit

---
+++ Number.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+class Number
+{
+	public static void main(String args[])
+	{
+		int no;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no");
+		no = sc.nextInt();
+		
+		if(no<0)
+		{
+		try
+			{
+				throw new  NegativeException();
+			}
+		catch(NegativeException e)
+			{
+				System.out.println(e);
+			}
+		}
+		else
+		{
+			if(no%2==0)
+				{
+					System.out.println(no+ " Is EVEN");
+				}
+			else
+				{
+					System.out.println(no+ " Is ODD");
+				}
+		}
+	}
+}
+class NegativeException extends Exception
+{
+	NegativeException()
+	{
+		System.out.println("NO Is Negative");
+	}
+}
